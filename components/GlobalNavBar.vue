e3d6f394108cf9fe80351b64a3cb4bd8a6da3843
dandanthemanman <85678575+dandanthemanman@users.noreply.github.com>
2023-10-04 14:01:51 +0200

    first pass contact and big name functionality

--- components/GlobalNavBar.vue
+++ components/GlobalNavBar.vue
@@ -1,17 +1,16 @@
 <template>
     <div class="nav-bar-container">
         <div class="name-container">
-            <h2>Dan Becker</h2>
+            <a class="big-name" href="/#intro">Dan Becker</a>
         </div>
         <div class="items-container">
             <div class="nav-bar-item">
-                <h3>projects</h3>
+                <a href="/#contact">Contact</a>
             </div>
-            <div class="nav-bar-item"></div>
-            <div class="nav-bar-item"></div>
-            <div class="nav-bar-item"></div>
+            <div class="nav-bar-item">
+                daniel.becker000@gmail.com
+            </div>
         </div>
-        
     </div>
 </template>
 
@@ -23,18 +22,30 @@
 .nav-bar-container{
     display: flex;
     justify-content: space-between;
+    a {
+    text-decoration: none;  
+    color: unset;
+    }
     .name-container {
         display: flex;
         justify-content: center;
         align-items: center;
         text-align: center;
+        .big-name{
+            color: white;
+            font-size: 45px;
+        }
     }
     .items-container{
         display: flex;
         .nav-bar-item {
+            display: flex;
+            justify-content: center;
+            align-items: center;
             border: 1px solid red;
             height: 50px;
-            width: 50px;
+            width: 25vw;
+            text-align: center;
         }  
     }
     
