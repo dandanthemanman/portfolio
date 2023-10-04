2c99af8402ce2004bdba70167a1c371560e3308b
dandanthemanman <85678575+dandanthemanman@users.noreply.github.com>
2023-10-04 16:46:08 +0200

    first pass layout/styling

--- pages/index.vue
+++ pages/index.vue
@@ -1,9 +1,11 @@
 <template>
-    <div id="intro">
-        <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Blanditiis nobis quisquam culpa ipsum! Adipisci hic doloribus nisi libero laudantium ducimus explicabo, deserunt exercitationem beatae, aliquid nulla optio! Expedita, necessitatibus quaerat.</p>
-        <p>Lorem, ipsum dolor sit amet consectetur adipisicing elit. Sunt corrupti eius fugiat magnam excepturi dignissimos aliquam id provident asperiores amet ab optio laborum accusamus, rerum assumenda corporis modi perspiciatis voluptatibus.</p>
-        <p>Lorem ipsum dolor sit, amet consectetur adipisicing elit. Inventore illo dolores reprehenderit asperiores tempore voluptatum, nisi saepe commodi? Voluptates explicabo possimus harum commodi temporibus vel nostrum amet corrupti enim architecto?</p>
-        <p>Lorem, ipsum dolor sit amet consectetur adipisicing elit. Beatae, soluta perferendis eligendi repudiandae ea optio possimus, nihil ab voluptatem officia iure facere cupiditate molestias animi quod et alias ad ipsa!</p>
+    <div class="about-projects-container" id="about">
+        <div class="about-container">
+            <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Consectetur atque harum reiciendis ea excepturi voluptas culpa doloribus quasi quisquam in perspiciatis, quod eius velit explicabo ullam labore nesciunt? Tenetur, vitae.</p>
+        </div>
+        <div class="projects-container">
+            <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Illo earum quod voluptate, doloremque adipisci odio maxime ab odit, optio sapiente minus. Fuga excepturi possimus magnam dolorum accusamus, vel ut doloremque!</p>
+        </div>
     </div>
 </template>
 
@@ -12,5 +14,33 @@
 </script>
 
 <style lang="scss" scoped>
-    
+    .about-projects-container {
+        border: 1px solid red;
+        display: grid;
+        grid-template-columns: repeat(2, 1fr);
+        height: 80vh;
+
+        .about-container {
+            padding: 1.4rem;
+            flex: 1;
+            display: flex;
+            flex-direction: column;
+            justify-content: flex-start;
+            p {
+                background-color: greenyellow;
+                font-family: $libre-franklin;
+            }
+        }
+        .projects-container {
+            padding: 1.4rem;
+            flex: 1;
+            display: flex;
+            flex-direction: column;
+            justify-content: flex-end;
+            p {
+                background-color: greenyellow;
+                font-family: $libre-franklin;
+            }
+        }
+    }
 </style>
