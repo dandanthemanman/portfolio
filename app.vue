f17fad143e025d13345373eea80106a7c75409f5
dandanthemanman <85678575+dandanthemanman@users.noreply.github.com>
2023-10-04 13:43:21 +0200

    first pass basic template

--- app.vue
+++ app.vue
@@ -1,6 +1,28 @@
 <template>
-  <div>
-    <NuxtWelcome />
-    extra code
+<div class="site">
+  <GlobalNavBar/>
+  <NuxtPage/>
+  <GlobalFooter/>
   </div>
 </template>
+
+<script setup>
+
+</script>
+
+<style lang="scss">
+.site {
+  background-image: url("./public/madrid_sunset.jpg");
+  background-repeat: no-repeat;
+  background-position: center;
+  background-size: cover;
+  display: flex;
+  min-height: 100vh;
+  flex-direction: column;
+  margin: 0;
+}
+
+.site-content {
+  flex: 1;
+}
+</style>
---
+++ components/GlobalNavBar.vue
@@ -0,0 +1,43 @@
+<template>
+    <div class="nav-bar-container">
+        <div class="name-container">
+            <h2>Dan Becker</h2>
+        </div>
+        <div class="items-container">
+            <div class="nav-bar-item">
+                <h3>projects</h3>
+            </div>
+            <div class="nav-bar-item"></div>
+            <div class="nav-bar-item"></div>
+            <div class="nav-bar-item"></div>
+        </div>
+        
+    </div>
+</template>
+
+<script setup>
+
+</script>
+
+<style lang="scss" scoped>
+.nav-bar-container{
+    display: flex;
+    justify-content: space-between;
+    .name-container {
+        display: flex;
+        justify-content: center;
+        align-items: center;
+        text-align: center;
+    }
+    .items-container{
+        display: flex;
+        .nav-bar-item {
+            border: 1px solid red;
+            height: 50px;
+            width: 50px;
+        }  
+    }
+    
+}
+
+</style>
