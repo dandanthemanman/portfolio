e8b27b1279f8f2388a025191b6db7cc14c6b4f13
dandanthemanman <85678575+dandanthemanman@users.noreply.github.com>
2023-10-05 17:59:48 +0200

    adds type-writer effect

--- pages/index.vue
+++ pages/index.vue
@@ -1,10 +1,10 @@
 <template>
     <div class="about-projects-container" id="about">
-        <div class="about-container">
-            <p>ABOUT <br> <br>Developer with one year's hands-on experience working at a web agency based out of LA</p>
+        <div class="about-container type-writer-effect">
+            <p>Developer with 1 year's hands-on experience building sites at LA based web agency </p>
         </div>
         <div class="projects-container">
-            <p>PROJECTS <br> <br>Lorem ipsum dolor sit amet consectetur adipisicing elit. Illo earum quod voluptate, doloremque adipisci odio maxime ab odit, optio sapiente minus. Fuga excepturi possimus magnam dolorum accusamus, vel ut doloremque!</p>
+            <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Illo earum quod voluptate, doloremque adipisci odio maxime ab odit, optio sapiente minus. Fuga excepturi possimus magnam dolorum accusamus, vel ut doloremque!</p>
         </div>
     </div>
     <div id="projects"></div>
@@ -21,7 +21,6 @@
         grid-template-columns: repeat(2, 1fr);
         height: 50vh;
         .about-container {
-            
             padding: 1.4rem;
             flex: 1;
             display: flex;
@@ -45,5 +44,25 @@
                 font-family: $libre-franklin;
             }
         }
+    }
+    .type-writer-effect p {
+        overflow: hidden; /* Ensures the content is not revealed until the animation */
+        border-right: .15em solid black; /* The typwriter cursor */
+        white-space: nowrap; /* Keeps the content on a single line */
+        margin: 0 auto; /* Gives that scrolling effect as the typing happens */
+        letter-spacing: .15em; /* Adjust as needed */
+        animation: 
+        typing 3.5s steps(40, end),
+        blink-caret .75s step-end infinite;
+    }
+
+    @keyframes typing {
+    from { width: 0 }
+    to { width: 100% }
+    }   
+
+    @keyframes blink-caret {
+    from, to { border-color: transparent }
+    50% { border-color: black; }
     }
 </style>
