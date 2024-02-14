3e4ff3da6ec943f1a729f42b8168cb3076f91348
dandanthemanman <85678575+dandanthemanman@users.noreply.github.com>
2024-02-14 14:03:21 -0600

    use ref to toggle menu open/close

--- components/BurgerMenu.vue
+++ components/BurgerMenu.vue
@@ -1,8 +1,9 @@
 <template>
-    <div @click="store.toggle">
-        <CloseIcon class="close-icon" v-if="store.burgerMenuOpen" @click="store.toggle"/>
-        <MenuIcon class="menu-icon" v-else/>
-        <h2 >{{ store.burgerMenuOpen }}</h2>
+    <div @click="toggleMenu" style="position: relative; height: 100px">
+         <div class="mobile-menu" :style="menuPosition">
+            <CloseIcon class="close-icon" />
+        </div>
+        <MenuIcon class="menu-icon"/>
     </div>
 </template>
 
@@ -10,9 +11,54 @@
 import MenuIcon from '@/components/MenuIcon.vue'
 import CloseIcon from '@/components/CloseIcon.vue'
 import { store } from "../store/store"
+import { FocusTrap } from 'focus-trap-vue'
+import { computed, ref } from 'vue'
+
+const menuOpen = ref(false)
+
+const toggleMenu = () => {
+    menuOpen.value = !menuOpen.value
+}
+
+const menuPosition = computed(() => {
+    return {
+        "transform": menuOpen.value ? "translateX(-400px)" : ""
+    }
+})
+
 </script>
 
 <style lang="scss" scoped>
+.mobile-menu {
+    position: absolute;
+    left: 80px;
+    height: 100vh;
+    width: 100vw;
+    z-index: 10;
+    background-color: red;
+    transition: mobile-menu-slide 4s;
+    .enter {
+        animation-duration: 0.5s;
+        animation-name: mobile-menu-slide;
+        animation-timing-function: cubic-bezier(0.37, 0, 0.63, 1);
+    }
+    .leave {
+        animation-direction: reverse;
+        animation-duration: 0.45s;
+        animation-fill-mode: both;
+        animation-name: mobile-menu-slide;
+        animation-timing-function: cubic-bezier(0.37, 0, 0.63, 1);
+    }
+}
+
+@keyframes mobile-menu-slide {
+  0% {
+    transform: translateX(100%);
+  }
+  100% {
+    transform: translateX(0);
+  }
+}
     .menu-icon {
         height: 40px;
         width: 40px;
--- store/store.js
+++ store/store.js
@@ -3,7 +3,7 @@ import { reactive } from 'vue'
 export const store = reactive({
     burgerMenuOpen: false, 
     toggle() {
-        
         this.burgerMenuOpen = !this.burgerMenuOpen
+        console.log('store.toggle')
     } 
 })
