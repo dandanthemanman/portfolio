f52af8e3b8886de26561f653adeb09a8a09aeb10
dandanthemanman <85678575+dandanthemanman@users.noreply.github.com>
2024-02-16 13:38:20 -0600

    drawers emit active drawer index

---
+++ components/WorkDrawer.vue
@@ -0,0 +1,71 @@
+<template>
+        <button @click="handleToggle" class="accordion">{{ drawerContent.title }}</button>
+            <div class="panel">
+            <p>{{ drawerContent.description }}</p>
+            <p>{{ `${activeDrawerIndex}` }}</p>
+            </div>
+</template>
+
+<script>
+export default {
+    props: {
+        activeDrawerIndex:  {
+            type: Number, 
+            default: null
+        },
+        drawerContent: {
+            type: Object, 
+            default: {}
+        }, 
+        index: {
+            type: Number,
+            default: null
+        }
+    },
+    emits: ['emitActiveDrawerIndex'],
+    setup () {
+        return {}
+    }, 
+    methods: {
+        handleToggle() {
+            console.log(`${this.$.vnode.key}`);
+            this.$emit('emitActiveDrawerIndex', this.$.vnode.key)
+    }
+    }, 
+    computed: {
+        isActive() {
+            return Boolean(activeDrawerIndex)
+        }
+  }
+}
+</script>
+
+<style lang="scss" scoped>
+
+.accordion {
+    font-size: 2rem;
+    font-family: $dot-gothic;
+  color: #444;
+  cursor: pointer;
+  padding: 18px;
+  width: 100%;
+  text-align: left;
+  border: none;
+  outline: none;
+  transition: 0.4s;
+  border-bottom: 1px solid white;
+}
+
+/* Add a background color to the button if it is clicked on (add the .active class with JS), and when you move the mouse over it (hover) */
+.accordion:hover {
+    background-color: rgb(59, 108, 173);
+    color: beige;
+}
+
+/* Style the accordion panel. Note: hidden by default */
+.panel {
+  padding: 0 18px;
+  background-color: white;
+  overflow: hidden;
+}
+</style>
--- components/WorkSection.vue
+++ components/WorkSection.vue
@@ -1,62 +1,54 @@
 <template>
-    <div id="work" class="work-section-container">
-        <button class="accordion">Dublab (Bean.la)</button>
-            <div class="panel">
-            <p>Lorem ipsum...</p>
-            </div>
-
-        <button class="accordion">Jaja Tequila (Bean.la)</button>
-            <div class="panel">
-            <p>Lorem ipsum...</p>
-            </div>
-
-        <button class="accordion">s94 Design (Bean.la)</button>
-            <div class="panel">
-            <p>Lorem ipsum...</p>
-            </div>
+    <div ref="accordionDrawers" id="work" class="work-section-container">
+        <WorkDrawer v-for="(workDrawer, index) in workDrawers"
+        :key="index"
+        :drawerContent = "workDrawer"
+        :active-drawer-index="activeDrawerIndex"
+        @emit-active-drawer-index="setActiveDrawerIndex"/>
     </div>
 </template>
 
 <script>
 export default {
-    setup () {
+    setup() {
+        const accordionDrawers = ref(null)
         
 
-        return {}
+        return {
+            accordionDrawers
+        }
+    },
+    data() {
+        return {
+            activeDrawerIndex: null, 
+            workDrawers: [
+                {
+                    title: "Dublab (Bean.la)",
+                    description: "lorem ipsum", 
+                },
+                {
+                    title: "Jaja Tequila (Bean.la)",
+                    description: "lorem ipsum2", 
+                },
+                {
+                    title: "s94 Design (Bean.la)",
+                    description: "lorem ipsum3", 
+                }
+            ]
+        }
+    },
+    methods: {
+        setActiveDrawerIndex(index) {
+            this.activeDrawerIndex =
+            index === this.activeDrawerIndex ? null : index
+        }
     }
 }
 </script>
 
 <style lang="scss" scoped>
-.work-section-container {
-}
-/* Style the buttons that are used to open and close the accordion panel */
-.accordion {
-    font-size: 2rem;
-    font-family: $dot-gothic;
-  color: #444;
-  cursor: pointer;
-  padding: 18px;
-  width: 100%;
-  text-align: left;
-  border: none;
-  outline: none;
-  transition: 0.4s;
-  border-bottom: 1px solid white;
-}
-
-/* Add a background color to the button if it is clicked on (add the .active class with JS), and when you move the mouse over it (hover) */
-.accordion:hover {
-    background-color: rgb(59, 108, 173);
-    color: beige;
-}
-
-/* Style the accordion panel. Note: hidden by default */
-.panel {
-  padding: 0 18px;
-  background-color: white;
-  display: none;
-  overflow: hidden;
+*{
+    z-index: 5;
 }
 
 </style>
