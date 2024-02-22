991651663da314cbff22b38cc16f7668c2801be1
dandanthemanman <85678575+dandanthemanman@users.noreply.github.com>
2024-02-22 12:56:07 -0600

    moves project drawer public imgs

--- assets/workDrawers.js
+++ assets/workDrawers.js
@@ -4,21 +4,21 @@ const workDrawers = [
         description: "Designed for Dublab radio, this audio player seamlessly integrates with the platform, allowing users to enjoy archived shows effortlessly. Developed with expertise in HTML, CSS, and JavaScript for smooth client-side interactions and enhanced user experience, it offers a user-friendly interface for accessing and listening to curated collections of past broadcasts.", 
         images: [
             // this url is not being recognized by netlify
-            { url: "./public/images/DublabPlayer.png", caption: "" }
+            { url: "/_nuxt/public/DublabPlayer.png", caption: "" }
         ]
     },
     {
         title: "Jaja Tequila (Bean.la)",
         description: "Jaja Tequila's website combines elegant design with essential business functionalities, requiring proficiency in HTML, CSS, JavaScript for client-side interactions, and back-end development for e-commerce capabilities. It showcases a seamless blend of aesthetic appeal and technical competence.", 
         images: [
-            { url: "/_nuxt/public/images/JajaLandingPage.png", caption: "" }
+            { url: "/_nuxt/public/JajaLandingPage.png", caption: "" }
         ]
     },
     {
         title: "s94 Design (Bean.la)",
         description: "Crafted for a high-end art gallery, this website elegantly merges a sophisticated CMS integration with bespoke front-end design. Expertise in CMS development, possibly utilizing platforms like WordPress or custom solutions, along with advanced front-end skills in HTML, CSS, and JavaScript, results in a seamless digital platform that mirrors the gallery's exclusivity and aesthetic refinement.",
         images: [
-            { url: "/_nuxt/public/images/s94Design.png", caption: "" }
+            { url: "/_nuxt/public/s94Design.png", caption: "" }
         ] 
     }
 ]
