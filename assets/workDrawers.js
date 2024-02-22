23f6c2d855aa660c8e859e5ade0e35729537342e
dandanthemanman <85678575+dandanthemanman@users.noreply.github.com>
2024-02-22 14:36:54 -0600

    removes 'url' and /public/ from img paths

--- assets/workDrawers.js
+++ assets/workDrawers.js
@@ -4,21 +4,21 @@ const workDrawers = [
         description: "Designed for Dublab radio, this audio player seamlessly integrates with the platform, allowing users to enjoy archived shows effortlessly. Developed with expertise in HTML, CSS, and JavaScript for smooth client-side interactions and enhanced user experience, it offers a user-friendly interface for accessing and listening to curated collections of past broadcasts.", 
         images: [
             // this url is not being recognized by netlify
-            { url: "url('./public/DublabPlayer.jpg')", caption: "" }
+            { url: '/DublabPlayer.jpg', caption: "" }
         ]
     },
     {
         title: "Jaja Tequila (Bean.la)",
         description: "Jaja Tequila's website combines elegant design with essential business functionalities, requiring proficiency in HTML, CSS, JavaScript for client-side interactions, and back-end development for e-commerce capabilities. It showcases a seamless blend of aesthetic appeal and technical competence.", 
         images: [
-            { url: "url('./public/JajaLandingPage.jpg')", caption: "" }
+            { url: '/JajaLandingPage.jpg', caption: "" }
         ]
     },
     {
         title: "s94 Design (Bean.la)",
         description: "Crafted for a high-end art gallery, this website elegantly merges a sophisticated CMS integration with bespoke front-end design. Expertise in CMS development, possibly utilizing platforms like WordPress or custom solutions, along with advanced front-end skills in HTML, CSS, and JavaScript, results in a seamless digital platform that mirrors the gallery's exclusivity and aesthetic refinement.",
         images: [
-            { url: "url('./public/s94Design.jpg')", caption: "" }
+            { url: '/s94Design.jpg', caption: "" }
         ] 
     }
 ]
